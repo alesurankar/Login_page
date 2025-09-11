f0f24a2d6461118d88a2a864df725ad1fae37949
ales2026 <ale.urankar@gmail.com>
2025-09-11 07:56:08 +0200

    hiding mobile menu

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const navbarMenu = document.querySelector(".navbar .links");
 const menuBtn = document.querySelector(".menu-btn");
+const hideMenuBtn = navbarMenu.querySelector(".close-btn");
 const showPopupBtn = document.querySelector(".login-btn");
 const formPopup = document.querySelector(".form-popup");
 const hidePopupBtn = document.querySelector(".form-popup .close-btn");
 const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
 
+// Show mobile menu
 menuBtn.addEventListener("click", () => {
     navbarMenu.classList.toggle("show-menu");
 });
+
+// Hide mobile menu
+hideMenuBtn.addEventListener("click", () => menuBtn.click());
 
 // Show form popup
 showPopupBtn.addEventListener("click", () => {
