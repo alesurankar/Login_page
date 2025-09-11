9b6f44f4379177378ab305034dd42fc5374ad1f7
ales2026 <ale.urankar@gmail.com>
2025-09-11 07:21:59 +0200

    toggling between login and signup

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const showPopupBtn = document.querySelector(".login-btn");
 const hidePopupBtn = document.querySelector(".form-popup .close-btn");
+const formPopup = document.querySelector(".form-popup");
+const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
 
 // Show form popup
 showPopupBtn.addEventListener("click", () => {
@@ -8,3 +10,10 @@ showPopupBtn.addEventListener("click", () => {
 
 // Hide form popup
 hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+loginSignupLink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
+    });
+});
