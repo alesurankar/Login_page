8f06ea524d70746d9aa77d89717e4edb90b1c28a
ales2026 <ale.urankar@gmail.com>
2025-09-11 07:02:12 +0200

    closing popup form

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const showPopupBtn = document.querySelector(".login-btn");
+const hidePopupBtn = document.querySelector(".form-popup .close-btn");
 
+// Show form popup
 showPopupBtn.addEventListener("click", () => {
     document.body.classList.toggle("show-popup");
 });
+
+// Hide form popup
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
