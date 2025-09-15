622096872ddf45766bf34f8b468a35931be438ae
ales2026 <ale.urankar@gmail.com>
2025-09-15 06:22:07 +0200

    adding favicon and users.js

---
+++ static/js/users.js
@@ -0,0 +1,29 @@
+const Users = (function () {
+    // Load users into a container
+    function loadUsers(container) {
+        fetch("/users")
+            .then(res => res.json())
+            .then(data => {
+                container.innerHTML = "";
+                if (data.users && data.users.length > 0) {
+                    const ul = document.createElement("ul");
+                    data.users.forEach(user => {
+                        const li = document.createElement("li");
+                        li.textContent = user;
+                        ul.appendChild(li);
+                    });
+                    container.appendChild(ul);
+                } else {
+                    container.textContent = "No users found";
+                }
+            })
+            .catch(err => {
+                console.error("Error fetching users:", err);
+                container.textContent = "Failed to load users";
+            });
+    }
+    return { 
+        loadUsers
+    };
+})();
+
